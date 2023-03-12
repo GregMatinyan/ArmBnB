6c2bb9bd0a964116302299bff321b3b300e078fa
Gregori Matinyan <gudzon437@gmail.com>
2023-03-12 14:57:48 +0400

    clean

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
 import "./App.css";
 
 function App() {
-<<<<<<< HEAD
   return <div className="App"></div>;
-=======
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          test="App-link-test"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> b577a85f159d9224f08cada619aa2fec378d1738
 }
 
 export default App;
