964ecb8ebd0d577e99949b8046da98f348afa0e8
Gregori Matinyan <gudzon437@gmail.com>
2023-04-09 17:09:24 +0400

    final commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,28 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Footer.module.css";
 import insta from "../../assets/icons/instagram.png";
 import facebook from "../../assets/icons/facebook.png";
 import twitter from "../../assets/icons/twitter.png";
-import google from "../../assets/icons/google-plus.png";
 import linkedin from "../../assets/icons/linkedin.png";
 
 function Footer() {
+  const [about, setAbout] = useState(false);
+  const [contact, setContact] = useState(false);
+
   return (
     <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div
+          style={{
+            width: "47%",
+            textAlign: "left",
+            paddingLeft: 15,
+            height: about ? 140 : 20,
+            borderRadius: 10,
+            backgroundColor: "antiquewhite",
+            visibility: about ? "visible" : "hidden",
+          }}
+        >
+          <p>
+            ARMBNB was born to help people organize their vacation correctly and
+            beautifully. At the same time ARMBNB helps people to find tenants
+            for their hosts. Every day, our dealers offer unique stays and
+            experiences that make it possible for guests to connect with
+            communities in a more authentic way. Pick your price range, the
+            number of rooms you want, and other key amenities to find the stay
+            that fits your needs. Every booking is protected!
+          </p>
+        </div>
+        <div
+          style={{
+            width: "30%",
+            textAlign: "left",
+            paddingLeft: 15,
+            height: contact ? 140 : 20,
+            borderRadius: 10,
+            backgroundColor: "antiquewhite",
+            visibility: contact ? "visible" : "hidden",
+          }}
+        >
+          <p>
+            Call us: <strong>+374-94-44-44-02 </strong>
+          </p>
+          <span>Or write to: </span>
+          <a href="mailto:aca.react.group1@gmail.com">
+            <strong>aca.react.group1@gmail.com </strong>
+          </a>
+        </div>
+      </div>
       <div className={styles.aboutContainer}>
         <div>
-          <p>ABOUT US</p>
+          <p style={{ cursor: "pointer" }} onClick={() => setAbout(!about)}>
+            ABOUT US
+          </p>
         </div>
         <div>
-          <p>CONTACT US</p>
+          <p style={{ cursor: "pointer" }} onClick={() => setContact(!contact)}>
+            CONTACT US
+          </p>
         </div>
         <div className={styles.linkContainer}>
           <span>FOLLOW US</span>
-          <img src={insta} alt="instagram" />
-          <img src={facebook} alt="facebook" />
-          <img src={linkedin} alt="linkedin" />
-          <img src={twitter} alt="twitter" />
-          <img src={google} alt="google" />
+          <a href="https://www.instagram.com">
+            <img src={insta} alt="ig" />
+          </a>
+          <a href="https://www.facebook.com">
+            <img src={facebook} alt="fb" />
+          </a>
+          <a href="https://www.linkedin.com">
+            <img src={linkedin} alt="in" />
+          </a>
+          <a href="https://www.twitter.com">
+            <img src={twitter} alt="twitter" />
+          </a>
         </div>
       </div>
       <div>
@@ -33,7 +88,6 @@ function Footer() {
             background: "black",
             color: "black",
             borderColor: "black",
-            // height: "0.1px",
           }}
         />
       </div>
